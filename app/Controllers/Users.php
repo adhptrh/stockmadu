7db2bc31d4cf1acf98054f5ae9f8088df0cce207
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-04-06 14:51:39 +0700

    change page sales to users

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -5,12 +5,12 @@ namespace App\Controllers;
 use App\Entities\User;
 use App\Models\UserModel;
 
-class Sales extends BaseController
+class Users extends BaseController
 {
     public function index()
     {
         if (session()->get("user_id")) {
-            return view("pages/sales");
+            return view("pages/users");
         }
         return view('auth/login');
     }
--- app/Views/layout/menu.php
+++ app/Views/layout/menu.php
@@ -8,10 +8,10 @@
     </a>
 </li>
 
-<li class="sidebar-item <?= (current_url() == base_url("/index.php")."/sales") ? "active":""; ?>">
-    <a href="<?= base_url("/sales") ?>" class='sidebar-link'>
+<li class="sidebar-item <?= (current_url() == base_url("/index.php")."/users") ? "active":""; ?>">
+    <a href="<?= base_url("/users") ?>" class='sidebar-link'>
         <i class="bi bi-people"></i>
-        <span>Sales</span>
+        <span>Users</span>
     </a>
 </li>
 
